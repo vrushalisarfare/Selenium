4c6243b912a0fc3b3100c3a2c53e6546be63b328
vrushalisarfare <87807056+vrushalisarfare@users.noreply.github.com>
2021-11-10 20:06:36 +0530

    Add files via upload

---
+++ MySelenium.java
@@ -0,0 +1,41 @@
+package MySelenium;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MySelenium {
+@Test
+	public void JenkinsTest() {
+			      	 
+	        // declaration and instantiation of objects/variables
+	    	System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+	    	
+	        String baseUrl = "http://demo.guru99.com/test/newtours/";
+	        String expectedTitle = "Welcome: Mercury Tours";
+	        String actualTitle = "";
+
+	        // launch Fire fox and direct it to the Base URL
+	        driver.get(baseUrl);
+
+	        // get the actual value of the title
+	        actualTitle = driver.getTitle();
+
+	        /*
+	         * compare the actual title of the page with the expected one and print
+	         * the result as "Passed" or "Failed"
+	         */
+	        if (actualTitle.contentEquals(expectedTitle)){
+	            System.out.println("Test Passed!");
+	        } else {
+	            System.out.println("Test Failed");
+	        }
+	       
+	        //close Fire fox
+	        driver.close();
+	       
+	}
+
+}
+
+
