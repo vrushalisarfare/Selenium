2441e66517ab2488454566d0e8efd7b6c083bbe4
vrushalisarfare <87807056+vrushalisarfare@users.noreply.github.com>
2021-11-10 20:09:59 +0530

    Update MySelenium.java

--- MySelenium.java
+++ MySelenium.java
@@ -21,11 +21,7 @@ public class MySelenium {
 	        // get the actual value of the title
 	        actualTitle = driver.getTitle();
 
-	        /*
-	         * compare the actual title of the page with the expected one and print
-	         * the result as "Passed" or "Failed"
-	         */
-	        if (actualTitle.contentEquals(expectedTitle)){
+	          if (actualTitle.contentEquals(expectedTitle)){
 	            System.out.println("Test Passed!");
 	        } else {
 	            System.out.println("Test Failed");
